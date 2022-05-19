db44bf6b017881534d44fb99ad25e84e2949f207
Dzmitry Miatelitsa <Dzmitry>
2022-05-19 12:40:28 +0300

    feat: finish task 1 read-file Thu, May 19, 2022 12:42:24 PM

---
+++ 01-read-file/index.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const path = require("path");
+let readableStream = fs.createReadStream(
+  path.join(__dirname, "text.txt"),
+  "utf-8"
+);
+
+readableStream.on("data", function (chunk) {
+  console.log(chunk);
+});
